d2517426ea48b121fffaaf995ecc81f53d725099
= <eloycolella@gmail.com>
2015-05-20 17:02:50 -0400

    agreguegado mapa y modificaciones del codigo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-app = angular.module('myApp', []);
+app = angular.module('myApp', ['ui.router', 'myApp.services']);
 
-app.controller("FarmaciaController", function($scope) {
-	
+app.controller("FarmaciaController", function($scope, FarmaciasSrv) {
+    
+    $scope.farmacias = FarmaciasSrv.list();
+    
 	$scope.x={ modo: 'DRIVING' };
 	
 	$scope.modoauto = function() 
@@ -14,424 +16,62 @@ app.controller("FarmaciaController", function($scope) {
 		$scope.x.modo = "WALKING";
 	};
 
-	$scope.farmacias=
-	[{
-        "nombre": "ARMELLINI",
-        "direccion": "Av. Central Y 31 Oeste",
-        "grupo": "A"
-    },
-    {
-        "nombre": "CABRERA",
-        "direccion": "Av. Falcón 222",
-        "grupo": "A"
-    },
-    {
-        "nombre": "DIAMANTE ",
-        "direccion": "Belgrano Y Alvarez",
-        "grupo": "A"
-    },
-    {
-        "nombre": "FURLAN ",
-        "direccion": "9 De Julio 260",
-        "grupo": "A"
-    },
-    {
-        "nombre": "GONZALEZ PACIN",
-        "direccion": "De La Nación 314",
-        "grupo": "A"
-    },
-    {
-        "nombre": "HECTOR LOPEZ ",
-        "direccion": "Maipú 749",
-        "grupo": "A"
-    },
-    {
-        "nombre": "CARRERA",
-        "direccion": "Almafuerte Y España",
-        "grupo": "B"
-    },
-    {
-        "nombre": "COCCARO",
-        "direccion": "Rivadavia 987 Bis",
-        "grupo": "B"
-    },
-    {
-        "nombre": "DEL PUEBLO",
-        "direccion": "De La Nación 450",
-        "grupo": "B"
-    },
-    {
-        "nombre": "DOTTO",
-        "direccion": "Pringles Y Alvear",
-        "grupo": "B"
-    },
-    {
-        "nombre": "MELONE",
-        "direccion": "Pte. Perón 858",
-        "grupo": "B"
-    },
-    {
-        "nombre": "TALJAME",
-        "direccion": "Alberdi 346",
-        "grupo": "B"
-    },
-    {
-        "nombre": "AMEFARMA SCS",
-        "direccion": "Mitre 200",
-        "grupo": "C"
-    },
-    {
-        "nombre": "GARETTO",
-        "direccion": "Av. Morteo Y España",
-        "grupo": "C"
-    },
-    {
-        "nombre": "LILIANA LATORRE",
-        "direccion": "Ameghino 347",
-        "grupo": "C"
-    },
-    {
-        "nombre": "PONCE",
-        "direccion": "Juramento 1445",
-        "grupo": "C"
-    },
-    {
-        "nombre": "RASETTO",
-        "direccion": "Viale 401",
-        "grupo": "C"
-    },
-    {
-        "nombre": "SAN NICOLÁS",
-        "direccion": "Cochabamba 357",
-        "grupo": "C"
-    },
-    {
-        "nombre": "BOFFA",
-        "direccion": "Av. Savio 1142",
-        "grupo": "C"
-    },
-    {
-        "nombre": "CEJ",
-        "direccion": "Maipú 495",
-        "grupo": "D"
-    },
-    {
-        "nombre": "DE LOS ARROYOS",
-        "direccion": "De La Nación 102",
-        "grupo": "D"
-    },
-    {
-        "nombre": "LOMBARDI",
-        "direccion": "Alberdi 548",
-        "grupo": "D"
-    },
-    {
-        "nombre": "PORTA",
-        "direccion": "Av. Savio 124",
-        "grupo": "D"
-    },
-    {
-        "nombre": "ROMERO",
-        "direccion": "Pte. Perón 1648",
-        "grupo": "D"
-    },
-    {
-        "nombre": "ALMADA",
-        "direccion": "Maipú Y J. B. Justo",
-        "grupo": "E"
-    },
-    {
-        "nombre": "CATALAN",
-        "direccion": "Almafuerte 442",
-        "grupo": "E"
-    },
-    {
-        "nombre": "DE LA TORRE",
-        "direccion": "Av. A. Illia 643",
-        "grupo": "E"
-    },
-    {
-        "nombre": "GIRARDI",
-        "direccion": "Av. Savio 1634",
-        "grupo": "E"
-    },
-    {
-        "nombre": "HENRICH",
-        "direccion": "9 De Julio 63",
-        "grupo": "E"
-    },
-    {
-        "nombre": "TONON",
-        "direccion": "Garibaldi 668",
-        "grupo": "E"
-    },
-    {
-        "nombre": "CANTONDEBAT",
-        "direccion": "Brown 598",
-        "grupo": "F"
-    },
-    {
-        "nombre": "CAVARA",
-        "direccion": "Italia Y Necochea",
-        "grupo": "F"
-    },
-    {
-        "nombre": "FENIX",
-        "direccion": "Garibaldi 281",
-        "grupo": "F"
-    },
-    {
-        "nombre": "FRATTINI",
-        "direccion": "Moreno 108",
-        "grupo": "F"
-    },
-    {
-        "nombre": "PRAT",
-        "direccion": "Pte. Peron 1093",
-        "grupo": "F"
-    },
-    {
-        "nombre": "RIVADAVIA ",
-        "direccion": "Rivadavia 724",
-        "grupo": "F"
-    },
-    {
-        "nombre": "ZONTA",
-        "direccion": "Urquiza 422",
-        "grupo": "F"
-    },
-    {
-        "nombre": "BARBOTTI",
-        "direccion": "Bolivar 351",
-        "grupo": "G"
-    },
-    {
-        "nombre": "BRASESCO",
-        "direccion": "Av. Savio Y Pombo",
-        "grupo": "G"
-    },
-    {
-        "nombre": "CESARI",
-        "direccion": "De La Nación 183",
-        "grupo": "G"
-    },
-    {
-        "nombre": "CONDE",
-        "direccion": "De La Nación 703",
-        "grupo": "G"
-    },
-    {
-        "nombre": "GARAGUSO",
-        "direccion": "Belgrano 320",
-        "grupo": "G"
-    },
-    {
-        "nombre": "PRINA",
-        "direccion": "Av. Arturo Ilia 739",
-        "grupo": "G"
-    },
-    {
-        "nombre": "BLANCO",
-        "direccion": "Almafuerte Y Benitez",
-        "grupo": "H"
-    },
-    {
-        "nombre": "CORREA ",
-        "direccion": "Italia 38",
-        "grupo": "H"
-    },
-    {
-        "nombre": "DONATELLI",
-        "direccion": "Urquiza 499",
-        "grupo": "H"
-    },
-    {
-        "nombre": "GAGLIARDO",
-        "direccion": "Pte. Perón 1035",
-        "grupo": "H"
-    },
-    {
-        "nombre": "SALVADOR",
-        "direccion": "Moreno 220",
-        "grupo": "H"
-    },
-    {
-        "nombre": "TIONI",
-        "direccion": "Rademil Y Alvear",
-        "grupo": "H"
-    },
-    {
-        "nombre": "TROFFE",
-        "direccion": "Av. Savio 238",
-        "grupo": "H"
-    },
-    {
-        "nombre": "CENTRO",
-        "direccion": "Don Bosco Y Pellegrini",
-        "grupo": "I"
-    },
-    {
-        "nombre": "CIMINARI",
-        "direccion": "Alberdi 699",
-        "grupo": "I"
-    },
-    {
-        "nombre": "GARCIA",
-        "direccion": "Belgrano 184",
-        "grupo": "I"
-    },
-    {
-        "nombre": "GOMEZ",
-        "direccion": "Pte. Peron 1366",
-        "grupo": "I"
-    },
-    {
-        "nombre": "LEONE",
-        "direccion": "Bolivar 1053",
-        "grupo": "I"
-    },
-    {
-        "nombre": "PZA. SARMIENTO ",
-        "direccion": "España Y Rivadavia",
-        "grupo": "I"
-    },
-    {
-        "nombre": "TONELLO",
-        "direccion": "Av. Falcón 651",
-        "grupo": "F"
-    },
-    {
-        "nombre": "ALLUCHON ",
-        "direccion": "Olleros 55",
-        "grupo": "J"
-    },
-    {
-        "nombre": "BONGIORNO ",
-        "direccion": "Francia Y Alberdi",
-        "grupo": "J"
-    },
-    {
-        "nombre": "BRACCO",
-        "direccion": "Av. Savio 373",
-        "grupo": "J"
-    },
-    {
-        "nombre": "FLOREANI",
-        "direccion": "Garibaldi Y Alem",
-        "grupo": "J"
-    },
-    {
-        "nombre": "PINASCO",
-        "direccion": "Alvear 95",
-        "grupo": "J"
-    },
-    {
-        "nombre": "PRADO M.",
-        "direccion": "Cernadas 110",
-        "grupo": "J"
-    },
-    {
-        "nombre": "ANDRADA",
-        "direccion": "Av. Savio 601",
-        "grupo": "K"
-    },
-    {
-        "nombre": "LALLO ",
-        "direccion": "Balcarce 441",
-        "grupo": "K"
-    },
-    {
-        "nombre": "MARIA I. LOPEZ",
-        "direccion": "L. Guruciaga 103",
-        "grupo": "K"
-    },
-    {
-        "nombre": "MARTINELLI",
-        "direccion": "Av. Moreno 74",
-        "grupo": "K"
-    },
-    {
-        "nombre": "PALAU",
-        "direccion": "Av. Central 2215",
-        "grupo": "K"
-    },
-    {
-        "nombre": "PERUGINI",
-        "direccion": "Lavalle 618",
-        "grupo": "K"
-    },
-    {
-        "nombre": "RADIUM",
-        "direccion": "De La Nación 352",
-        "grupo": "K"
-    },
-    {
-        "nombre": "BARONI",
-        "direccion": "Av. Irigoyen 1272",
-        "grupo": "L"
-    },
-    {
-        "nombre": "CAPRA",
-        "direccion": "Moreno 446",
-        "grupo": "L"
-    },
-    {
-        "nombre": "HEGOUABURU",
-        "direccion": "Hegouaburu",
-        "grupo": "L"
-    },
-    {
-        "nombre": "MACCARONI",
-        "direccion": "Av. Savio 717",
-        "grupo": "L"
-    },
-    {
-        "nombre": "MENNA",
-        "direccion": "Rivadavia 501",
-        "grupo": "L"
-    },
-    {
-        "nombre": "PUCCIARELLI",
-        "direccion": "Lavalle 215 Bis",
-        "grupo": "L"
-    }];
+	
 });
 
 app.directive('ngDistancia', function($q , $timeout) {
-	
+    var directionsService = new google.maps.DirectionsService();
 	var coords;
-	
+
+	var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+    
 	function getCurrentPosition(){
 		var d = $q.defer();
 		if (navigator.geolocation) {
-			if(coords === undefined) {
-		    	navigator.geolocation.getCurrentPosition(function(position)	{
-		    		coords = position.coords;
-					d.resolve(position.coords);
-		    	});
-			}else{
-				d.resolve(coords);
-			}
+		    if(coords === undefined) {
+                navigator.geolocation.getCurrentPosition(function(pos) {
+                    d.resolve(pos.coords);
+                }, function error(err) {
+                    console.warn('ERROR(' + err.code + '): ' + err.message);
+                    d.resolve(err);
+                }, options);
+            }
 	  	}
 	  	return d.promise;
 	}
 
 	
 	function distancia(direccion , modo, posicionActual) {
+		var request;
 		var d = $q.defer();
 		
 		getCurrentPosition().then(function(posicionActual) {
 			var origen = posicionActual.latitude + ',' + posicionActual.longitude; 
 			var destino = direccion + ', San Nicolás de Los Arroyos, Buenos Aires, Argentina';
 			var service = new google.maps.DistanceMatrixService();
-			service.getDistanceMatrix ({
+			var request = {
 				origins: [origen],
 				destinations: [destino],
 				travelMode: google.maps.TravelMode[modo],
 				unitSystem: google.maps.UnitSystem.METRIC,
 				avoidHighways: false,
 				avoidTolls: false
-			}, callback);
+			};
+			var origen2 = new google.maps.LatLng(posicionActual.latitude + ',' + posicionActual.longitude); 
+			var destino2 = direccion + ', San Nicolás de Los Arroyos, Buenos Aires, Argentina';
+            var request2 = {
+				origin: origen,
+				destination: destino,
+				travelMode: google.maps.TravelMode[modo],
+				unitSystem: google.maps.UnitSystem.METRIC
+			};
+			
+			//obtengo distancias
+			service.getDistanceMatrix (request, callback);
 			
 			function callback(response, status) {
 			if (status != google.maps.DistanceMatrixStatus.OK) {
@@ -442,10 +82,21 @@ app.directive('ngDistancia', function($q , $timeout) {
 				var datos = {};
 			    datos.distancia = results[0].distance.text;
 			    datos.duracion = results[0].duration.text;
+			    
+                //dibujo recorrido
+                directionsService.route(request2, function(response, status) {
+                    if (status == google.maps.DirectionsStatus.OK) {
+                      directionsDisplay.setDirections(response);
+                    }
+                });
+			    
 			    d.resolve(datos);
 			  }
 			}
-		});
+			
+        			
+            });
+        
 		return d.promise;
 	}
 	
@@ -458,6 +109,17 @@ app.directive('ngDistancia', function($q , $timeout) {
 		template: 'distancia : {{ distancia }} , duracion : {{ duracion }}',
 		link: function(scope) {
 				google.maps.event.addDomListener(window, 'load', function(){
+				    
+                directionsDisplay = new google.maps.DirectionsRenderer();
+                var sn = new google.maps.LatLng(-33.3388215,-60.2195587);
+                var mapOptions = {
+                    zoom:12,
+                    center: sn
+                };
+                map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+                directionsDisplay.setMap(map);
+                
+
 				// Calcula distancia de posicionActual hacia la direccion del elemento farmacia
 				distancia(scope.direccion, scope.modo).then(function(datos){
 					scope.distancia = datos.distancia;	
---
+++ js/services.js
@@ -0,0 +1,392 @@
+angular.module('myApp.services', [])
+
+.factory('FarmaciasSrv', function(){
+    
+    var farmacias =
+	[{
+        "nombre": "ARMELLINI",
+        "direccion": "Av. Central Y 31 Oeste",
+        "grupo": "A"
+    },
+    {
+        "nombre": "CABRERA",
+        "direccion": "Av. Falcón 222",
+        "grupo": "A"
+    },
+    {
+        "nombre": "DIAMANTE ",
+        "direccion": "Belgrano Y Alvarez",
+        "grupo": "A"
+    },
+    {
+        "nombre": "FURLAN ",
+        "direccion": "9 De Julio 260",
+        "grupo": "A"
+    },
+    {
+        "nombre": "GONZALEZ PACIN",
+        "direccion": "De La Nación 314",
+        "grupo": "A"
+    },
+    {
+        "nombre": "HECTOR LOPEZ ",
+        "direccion": "Maipú 749",
+        "grupo": "A"
+    },
+    {
+        "nombre": "CARRERA",
+        "direccion": "Almafuerte Y España",
+        "grupo": "B"
+    },
+    {
+        "nombre": "COCCARO",
+        "direccion": "Rivadavia 987 Bis",
+        "grupo": "B"
+    },
+    {
+        "nombre": "DEL PUEBLO",
+        "direccion": "De La Nación 450",
+        "grupo": "B"
+    },
+    {
+        "nombre": "DOTTO",
+        "direccion": "Pringles Y Alvear",
+        "grupo": "B"
+    },
+    {
+        "nombre": "MELONE",
+        "direccion": "Pte. Perón 858",
+        "grupo": "B"
+    },
+    {
+        "nombre": "TALJAME",
+        "direccion": "Alberdi 346",
+        "grupo": "B"
+    },
+    {
+        "nombre": "AMEFARMA SCS",
+        "direccion": "Mitre 200",
+        "grupo": "C"
+    },
+    {
+        "nombre": "GARETTO",
+        "direccion": "Av. Morteo Y España",
+        "grupo": "C"
+    },
+    {
+        "nombre": "LILIANA LATORRE",
+        "direccion": "Ameghino 347",
+        "grupo": "C"
+    },
+    {
+        "nombre": "PONCE",
+        "direccion": "Juramento 1445",
+        "grupo": "C"
+    },
+    {
+        "nombre": "RASETTO",
+        "direccion": "Viale 401",
+        "grupo": "C"
+    },
+    {
+        "nombre": "SAN NICOLÁS",
+        "direccion": "Cochabamba 357",
+        "grupo": "C"
+    },
+    {
+        "nombre": "BOFFA",
+        "direccion": "Av. Savio 1142",
+        "grupo": "C"
+    },
+    {
+        "nombre": "CEJ",
+        "direccion": "Maipú 495",
+        "grupo": "D"
+    },
+    {
+        "nombre": "DE LOS ARROYOS",
+        "direccion": "De La Nación 102",
+        "grupo": "D"
+    },
+    {
+        "nombre": "LOMBARDI",
+        "direccion": "Alberdi 548",
+        "grupo": "D"
+    },
+    {
+        "nombre": "PORTA",
+        "direccion": "Av. Savio 124",
+        "grupo": "D"
+    },
+    {
+        "nombre": "ROMERO",
+        "direccion": "Pte. Perón 1648",
+        "grupo": "D"
+    },
+    {
+        "nombre": "ALMADA",
+        "direccion": "Maipú Y J. B. Justo",
+        "grupo": "E"
+    },
+    {
+        "nombre": "CATALAN",
+        "direccion": "Almafuerte 442",
+        "grupo": "E"
+    },
+    {
+        "nombre": "DE LA TORRE",
+        "direccion": "Av. A. Illia 643",
+        "grupo": "E"
+    },
+    {
+        "nombre": "GIRARDI",
+        "direccion": "Av. Savio 1634",
+        "grupo": "E"
+    },
+    {
+        "nombre": "HENRICH",
+        "direccion": "9 De Julio 63",
+        "grupo": "E"
+    },
+    {
+        "nombre": "TONON",
+        "direccion": "Garibaldi 668",
+        "grupo": "E"
+    },
+    {
+        "nombre": "CANTONDEBAT",
+        "direccion": "Brown 598",
+        "grupo": "F"
+    },
+    {
+        "nombre": "CAVARA",
+        "direccion": "Italia Y Necochea",
+        "grupo": "F"
+    },
+    {
+        "nombre": "FENIX",
+        "direccion": "Garibaldi 281",
+        "grupo": "F"
+    },
+    {
+        "nombre": "FRATTINI",
+        "direccion": "Moreno 108",
+        "grupo": "F"
+    },
+    {
+        "nombre": "PRAT",
+        "direccion": "Pte. Peron 1093",
+        "grupo": "F"
+    },
+    {
+        "nombre": "RIVADAVIA ",
+        "direccion": "Rivadavia 724",
+        "grupo": "F"
+    },
+    {
+        "nombre": "ZONTA",
+        "direccion": "Urquiza 422",
+        "grupo": "F"
+    },
+    {
+        "nombre": "BARBOTTI",
+        "direccion": "Bolivar 351",
+        "grupo": "G"
+    },
+    {
+        "nombre": "BRASESCO",
+        "direccion": "Av. Savio Y Pombo",
+        "grupo": "G"
+    },
+    {
+        "nombre": "CESARI",
+        "direccion": "De La Nación 183",
+        "grupo": "G"
+    },
+    {
+        "nombre": "CONDE",
+        "direccion": "De La Nación 703",
+        "grupo": "G"
+    },
+    {
+        "nombre": "GARAGUSO",
+        "direccion": "Belgrano 320",
+        "grupo": "G"
+    },
+    {
+        "nombre": "PRINA",
+        "direccion": "Av. Arturo Ilia 739",
+        "grupo": "G"
+    },
+    {
+        "nombre": "BLANCO",
+        "direccion": "Almafuerte Y Benitez",
+        "grupo": "H"
+    },
+    {
+        "nombre": "CORREA ",
+        "direccion": "Italia 38",
+        "grupo": "H"
+    },
+    {
+        "nombre": "DONATELLI",
+        "direccion": "Urquiza 499",
+        "grupo": "H"
+    },
+    {
+        "nombre": "GAGLIARDO",
+        "direccion": "Pte. Perón 1035",
+        "grupo": "H"
+    },
+    {
+        "nombre": "SALVADOR",
+        "direccion": "Moreno 220",
+        "grupo": "H"
+    },
+    {
+        "nombre": "TIONI",
+        "direccion": "Rademil Y Alvear",
+        "grupo": "H"
+    },
+    {
+        "nombre": "TROFFE",
+        "direccion": "Av. Savio 238",
+        "grupo": "H"
+    },
+    {
+        "nombre": "CENTRO",
+        "direccion": "Don Bosco Y Pellegrini",
+        "grupo": "I"
+    },
+    {
+        "nombre": "CIMINARI",
+        "direccion": "Alberdi 699",
+        "grupo": "I"
+    },
+    {
+        "nombre": "GARCIA",
+        "direccion": "Belgrano 184",
+        "grupo": "I"
+    },
+    {
+        "nombre": "GOMEZ",
+        "direccion": "Pte. Peron 1366",
+        "grupo": "I"
+    },
+    {
+        "nombre": "LEONE",
+        "direccion": "Bolivar 1053",
+        "grupo": "I"
+    },
+    {
+        "nombre": "PZA. SARMIENTO ",
+        "direccion": "España Y Rivadavia",
+        "grupo": "I"
+    },
+    {
+        "nombre": "TONELLO",
+        "direccion": "Av. Falcón 651",
+        "grupo": "F"
+    },
+    {
+        "nombre": "ALLUCHON ",
+        "direccion": "Olleros 55",
+        "grupo": "J"
+    },
+    {
+        "nombre": "BONGIORNO ",
+        "direccion": "Francia Y Alberdi",
+        "grupo": "J"
+    },
+    {
+        "nombre": "BRACCO",
+        "direccion": "Av. Savio 373",
+        "grupo": "J"
+    },
+    {
+        "nombre": "FLOREANI",
+        "direccion": "Garibaldi Y Alem",
+        "grupo": "J"
+    },
+    {
+        "nombre": "PINASCO",
+        "direccion": "Alvear 95",
+        "grupo": "J"
+    },
+    {
+        "nombre": "PRADO M.",
+        "direccion": "Cernadas 110",
+        "grupo": "J"
+    },
+    {
+        "nombre": "ANDRADA",
+        "direccion": "Av. Savio 601",
+        "grupo": "K"
+    },
+    {
+        "nombre": "LALLO ",
+        "direccion": "Balcarce 441",
+        "grupo": "K"
+    },
+    {
+        "nombre": "MARIA I. LOPEZ",
+        "direccion": "L. Guruciaga 103",
+        "grupo": "K"
+    },
+    {
+        "nombre": "MARTINELLI",
+        "direccion": "Av. Moreno 74",
+        "grupo": "K"
+    },
+    {
+        "nombre": "PALAU",
+        "direccion": "Av. Central 2215",
+        "grupo": "K"
+    },
+    {
+        "nombre": "PERUGINI",
+        "direccion": "Lavalle 618",
+        "grupo": "K"
+    },
+    {
+        "nombre": "RADIUM",
+        "direccion": "De La Nación 352",
+        "grupo": "K"
+    },
+    {
+        "nombre": "BARONI",
+        "direccion": "Av. Irigoyen 1272",
+        "grupo": "L"
+    },
+    {
+        "nombre": "CAPRA",
+        "direccion": "Moreno 446",
+        "grupo": "L"
+    },
+    {
+        "nombre": "HEGOUABURU",
+        "direccion": "Hegouaburu",
+        "grupo": "L"
+    },
+    {
+        "nombre": "MACCARONI",
+        "direccion": "Av. Savio 717",
+        "grupo": "L"
+    },
+    {
+        "nombre": "MENNA",
+        "direccion": "Rivadavia 501",
+        "grupo": "L"
+    },
+    {
+        "nombre": "PUCCIARELLI",
+        "direccion": "Lavalle 215 Bis",
+        "grupo": "L"
+    }];
+    
+    function farmaciaList(){
+        return farmacias;
+    }
+    
+    return { list: farmaciaList }
+})
